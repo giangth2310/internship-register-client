1546642ee7d66aa7100d78b91d80aa0b3a236ac6
giangth2310 <giangth2310@gmail.com>
2018-05-06 11:16:21 +0700

    tao trang signin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
+import { create } from "jss";
+import JssProvider from "react-jss/lib/JssProvider";
+import { createGenerateClassName, jssPreset } from "material-ui/styles";
+
+import Signin from './containers/Signin/Signin';
+
+const generateClassName = createGenerateClassName();
+const jss = create(jssPreset());
+jss.options.insertionPoint = "insertion-point-jss";
+
 class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <JssProvider jss={jss} generateClassName={generateClassName}>
+        <Signin />
+      </JssProvider>
     );
   }
 }
---
+++ src/containers/Signin/Signin.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+import Button from 'material-ui/Button';
+import classes from './Signin.css';
+import TextField from 'material-ui/TextField';
+import Paper from 'material-ui/Paper';
+import Logo from '../../assets/images/logo.png';
+import Typography from 'material-ui/Typography';
+import Grid from 'material-ui/Grid';
+
+class Signin extends Component {
+    render () {
+        return (
+            <Grid container className={classes.Signin} justify='center' alignItems='center' >
+                <Grid item xs={12} sm={5} lg={4} >
+                    <Paper className={classes.Paper}>
+                        <Grid container className={classes.header}>
+                            <Grid item xs={12} md={3}>
+                                <Grid container justify='center' >
+                                    <img src={Logo} alt='logo' className={classes.Logo} />
+                                </Grid>
+                            </Grid>
+                            <Grid item xs={12} md={9}>
+                                <Typography variant = 'display1'
+                                    gutterBottom
+                                    align='center' >
+                                    Hệ thống <br/> đăng ký thực tập
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        <TextField 
+                            fullWidth 
+                            id='username'
+                            label='Tên đăng nhập'
+                            margin='normal' />
+                        <TextField 
+                            fullWidth 
+                            id='password'
+                            label='Mật khẩu' 
+                            type='password'
+                            margin='normal' />
+                        <Button variant="raised" color="primary" className={classes.Button}>Đăng nhập</Button>
+                    </Paper>
+                </Grid>
+            </Grid>
+        );
+    }
+}
+
+export default Signin;
